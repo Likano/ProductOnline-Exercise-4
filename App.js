fa897ee18092c03aa8e6e5fafaf009acc65fc60f
Likano <117450946+Likano@users.noreply.github.com>
2023-05-01 23:57:53 +0200

    App.js

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import { StyleSheet, Text, View,StatusBar,ScrollView} from 'react-native';
 import Picture from './Components/Picture';
 import Prices from './Components/Prices';
+import Header from './Components/Header';
 
-export default function App() {
+
+
+export default function App (){
+  
   return (
     <View style={styles.container}>
       <StatusBar />
+      <Header />
+    <ScrollView>
       <Picture />
       <Prices />
+    </ScrollView>
     </View>
   );
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column"
+    flexDirection: "column",
+    backgroundColor: "#e3f2fd"
   },
 });
